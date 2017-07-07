58526dd57f8b639e7a96862d1d14186294db2b51
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-06 21:24:08 -0400

    initial code

---
+++ src/commit-closes-spec.js
@@ -0,0 +1,11 @@
+'use strict'
+
+
+/* global describe, it */
+const commitCloses = require('.')
+
+describe('commit-closes', () => {
+  it('write this test', () => {
+    console.assert(commitCloses, 'should export something')
+  })
+})
