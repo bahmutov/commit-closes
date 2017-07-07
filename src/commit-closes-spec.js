d51492e9fd22eedab54cc5f9bd2215843d470d9a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-06 21:33:02 -0400

    start testing

--- src/commit-closes-spec.js
+++ src/commit-closes-spec.js
@@ -1,11 +1,15 @@
 'use strict'
 
+const snapshot = require('snap-shot')
 
 /* global describe, it */
 const commitCloses = require('.')
 
+// parsing tests adapted from
+// https://github.com/conventional-changelog/conventional-changelog
 describe('commit-closes', () => {
-  it('write this test', () => {
-    console.assert(commitCloses, 'should export something')
+  it('nothing closed', () => {
+    const issues = commitCloses('this is subject line', 'this is commit body')
+    snapshot(issues)
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 'use strict'
 
+const la = require('lazy-ass')
+const is = require('check-more-types')
 
-module.exports = true
+function commitCloses (subject, body) {
+  la(is.unemptyString(subject), 'commit is missing subject', subject)
+  la(is.maybe.unemptyString(body), 'wrong commit body', body)
 
+  const list = []
+  return list
+}
+
+module.exports = commitCloses
