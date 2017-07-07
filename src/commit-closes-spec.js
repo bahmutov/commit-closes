85e99f15b23e665a06c438f5e6d99914b334c938
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-07-06 21:41:27 -0400

    more tests

--- src/commit-closes-spec.js
+++ src/commit-closes-spec.js
@@ -24,4 +24,37 @@ describe('commit-closes', () => {
       'this is commit body')
     snapshot(issues)
   })
+
+  it('finds multiple issues in body on separate lines', () => {
+    const issues = commitCloses('this is subject line',
+      `
+      this is commit body
+      blah blah blah
+      Closes #123
+      Closes #25
+      Fixes #33
+      `
+    )
+    snapshot(issues)
+  })
+
+  it('finds multiple issues in body same line', () => {
+    const issues = commitCloses('this is subject line',
+      `this is commit body
+      blah blah blah
+      Closes #123 Closes #25 Fixes #32`
+    )
+    snapshot(issues)
+  })
+
+  it('finds multiple issues in body with dots', () => {
+    const issues = commitCloses('this is subject line',
+      `
+      this is commit body
+      blah blah blah
+      Closes #2, #3. Resolves #4. Fixes #5. Fixes #6.
+      and another line`
+    )
+    snapshot(issues)
+  })
 })
